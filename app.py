8dc8c0281da96a1ed12fd01d548ff45bc29f1af4
Sarah Heavner <sheavner@gmail.com>
2020-11-17 18:25:01 -0700

    completed stations section

--- app.py
+++ app.py
@@ -36,6 +36,23 @@ def home():
         f'/api/v1.0/start_end<br/>'
     )
 
+@app.route('/api/v1.0/stations')
+def stations():
+    session = Session(engine)
+    
+    results = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    all_stations = []
+    for station, name in results:
+        station_dict = {}
+        station_dict['station'] = station
+        station_dict['name'] = name
+        all_stations.append(station_dict)
+
+    return jsonify(all_stations)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
