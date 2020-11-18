80f851f6140c049ad66cd693dc78b2469f43bb2a
Sarah Heavner <sheavner@gmail.com>
2020-11-17 17:47:21 -0700

    started app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
--
+#Import dependencies
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#Database setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
