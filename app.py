56b3f159880f2fe2110393802aeee41dc4016e81
Sarah Heavner <sheavner@gmail.com>
2020-11-18 19:58:33 -0700

    corrected tobs section

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ Station = Base.classes.station
 app = Flask(__name__)
 
 #Set Flask Routes
+#Create route to home
 @app.route('/')
 def home():
     """list all available API routes"""
@@ -35,20 +36,25 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start/end<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/start/&lt;start&gt;/&lt;end&gt;<br/>"
     )
 
 
+#Create route for precipitation data
 @app.route('/api/v1.0/precipitation')  
 def precipitation():
     """Convert the query results to a dictionary using date as the key and prcp as the value"""
+    #Create session between Python and database
     session = Session(engine)
 
+    #Query for date and precipitation measurement
     precip = session.query(Measurement.date, Measurement.prcp).all()
 
+    #Close session
     session.close()
 
+    #Create list / dictionary to store results
     all_precip = []
     for date, prcp in precip:
         precip_dict={}
@@ -56,64 +62,88 @@ def precipitation():
         precip_dict['prcp'] = prcp
         all_precip.append(precip_dict)
 
+    #Return responses in JSON format
     return jsonify(all_precip)
 
 
-
+#Create route for stations
 @app.route('/api/v1.0/stations')
 def stations():
     """Return a JSON list of stations from the dataset."""
+    #Create session between Python and database
     session = Session(engine)
-    
-    results = session.query(Station.station, Station.name).all()
 
+    #Query for station and name
+    station_info = session.query(Station.station, Station.name).all()
+
+    #Close session
     session.close()
 
+    #Create list / dictionary to store results
     all_stations = []
-    for station, name in results:
+    for station, name in station_info:
         station_dict = {}
         station_dict['station'] = station
         station_dict['name'] = name
         all_stations.append(station_dict)
 
+    #Return results in JSON format
     return jsonify(all_stations)
 
 
+#Create route for tobs
 @app.route('/api/v1.0/tobs')
 def tobs():
     """Query the dates and temperature observations of the most active station for the last year of data."""
+    #Create session between Python and database
     session = Session(engine)
 
     #Find most active station
-    most_active_station = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count().desc()).first()
-    (most_active, ) = most_active_station
+    most_active_station = session.query(Measurement.station)\
+                          .group_by(Measurement.station)\
+                          .order_by(func.count().desc()).first()
+    most_active = most_active_station[0]
 
-    #Find date range
-    recent_date_most_active = session.query(Measurement.date).filter(Measurement.station == most_active).order_by(Measurement.date.desc()).first()
-    (most_recent_date, ) = recent_date_most_active
+    #Find most recent date and date 1 year ago for most active station
+    recent_date_most_active = session.query(Measurement.date)\
+                              .filter(Measurement.station == most_active)\
+                              .order_by(Measurement.date.desc()).first()
 
-    most_recent_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
-    most_recent_date = most_recent_date.date()
-    twelve_months =  most_recent_date - dt.timedelta(days=365)
+    twelve_months = (dt.datetime.strptime(recent_date_most_active[0],'%Y-%m-%d') \
+                   - dt.timedelta(days=365)).strftime('%Y-%m-%d')
 
-    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == most_active).filter(Measurement.date >= twelve_months).all()
-
+    #Query for date and temperature using most active station and date range from above
+    tobs = session.query(Measurement.date, Measurement.tobs)\
+           .filter(Measurement.station == most_active)\
+           .filter(Measurement.date >= twelve_months).all()
+   
+    #Close session
     session.close()
 
+    #Create list / dictionary to store results
     all_tobserv = []
-    for date, tobs in all_tobserv:
+    for date, tobs in tobs:
         all_tobserv_dict = {}
         all_tobserv_dict['date'] = date
         all_tobserv_dict['tobs'] = tobs
         all_tobserv.append(all_tobserv_dict)
 
+    #Return results in JSON format
     return jsonify(all_tobserv)
 
 
+#Create route for TMIN, TAVG, and TMAX per date starting from a starting date
 @app.route('/api/v1.0/start')
 def start():
 
     session = Session(engine)
+
+    start_tobs = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+
+    session.close()
+
+    return jsonify(start_tobs)
 
 
 
